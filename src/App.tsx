6a210b48aa54113d422d241c1e463fbd6aede878
Yamaguchi Hiromitsu <yamagame@mac.com>
2021-06-10 08:32:55 +0900

    first init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+declare const webkitSpeechRecognition: typeof SpeechRecognition;
+
+const speechRecognition = (() => {
+  if ("webkitSpeechRecognition" in window) {
+    return new webkitSpeechRecognition();
+  }
+})();
+
+const Button: React.FC<{ label: string; onClick: React.MouseEventHandler }> = ({
+  label,
+  onClick,
+}) => {
+  return (
+    <button
+      className="border rounded-lg p-2 focus:outline-none"
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
 
 function App() {
+  const [startRecognition, setStartRecognition] = React.useState(false);
+  const [result, setResult] = React.useState("");
+  React.useEffect(() => {
+    if (speechRecognition !== undefined) {
+      if (startRecognition) {
+        speechRecognition.onresult = (e) => {
+          setResult(e.results[0][0].transcript);
+        };
+        speechRecognition.onend = () => {
+          setStartRecognition(false);
+        };
+        speechRecognition.start();
+      } else {
+        speechRecognition.stop();
+      }
+    }
+  }, [startRecognition]);
+  const toggleStartRecognition = React.useCallback(() => {
+    setStartRecognition((v) => !v);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Button
+        label={startRecognition ? "Stop Recognition" : "Start Recognition"}
+        onClick={toggleStartRecognition}
+      ></Button>
+      <div>{result}</div>
     </div>
   );
 }
