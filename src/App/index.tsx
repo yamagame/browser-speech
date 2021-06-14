0b158bada64325fc0f2f34799a3d377dbfe9e5d9
Yamaguchi Hiromitsu <yamagame@mac.com>
2021-06-15 08:21:21 +0900

    hook の整理

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import axios from "axios";
-import SockJS from "sockjs-client";
-import { speechRecognition, speechSynthesis } from "./usecase";
-import { ControlMessage } from "./model";
+import {
+  authCheck,
+  processControl,
+  processRecognition,
+  speechSynthesis,
+} from "./usecase";
 import { Route } from "react-router-dom";
 import { Login } from "pages/Login";
 import { Presentation } from "pages/Presentation";
@@ -18,71 +21,17 @@ function App() {
   const [loading, setLoading] = React.useState(true);
 
   // 未ログインならログインページへ遷移
-  React.useEffect(() => {
-    const auth = async () => {
-      try {
-        const res = await axios.post("/auth");
-        if (res.data.username) {
-          setUsername(res.data.username);
-        } else {
-          history.push("/login");
-        }
-      } catch (err) {
-        history.push("/login");
-      }
-      setLoading(false);
-    };
-    if (loading) auth();
-  }, [loading, history]);
+  React.useEffect(
+    () => authCheck({ loading, history, setUsername, setLoading }),
+    [loading, history]
+  );
 
   // サーバーからのコントロールメッセージを処理
-  React.useEffect(() => {
-    const sock = new SockJS("/controller");
-    const onmessage = async (e: MessageEvent) => {
-      const data = JSON.parse(e.data) as ControlMessage;
-      switch (data.action) {
-        case "text-to-speech":
-          setResult(data.utterance);
-          await speechSynthesis(data.utterance);
-          await axios.post("/ready");
-          break;
-        case "speech-to-text/start":
-          setStartRecognition(true);
-          setResult("");
-          break;
-        case "speech-to-text/stop":
-          setStartRecognition(false);
-          setResult("");
-          break;
-      }
-    };
-    sock.addEventListener("message", onmessage);
-    return () => {
-      sock.removeEventListener("message", onmessage);
-    };
-  }, []);
+  React.useEffect(() => processControl({ setResult, setStartRecognition }), []);
 
   // 音声認識
   React.useEffect(() => {
-    if (speechRecognition !== undefined) {
-      if (startRecognition) {
-        speechRecognition.onresult = async (e) => {
-          setResult(e.results[0][0].transcript);
-          // 認識結果を送信
-          await axios.post("/transcript", {
-            transcript: e.results[0][0].transcript,
-          });
-        };
-        speechRecognition.onend = async () => {
-          setStartRecognition(false);
-          // 音声認識終了通知
-          await axios.post("/ready");
-        };
-        speechRecognition.start();
-      } else {
-        speechRecognition.stop();
-      }
-    }
+    processRecognition({ startRecognition, setResult, setStartRecognition });
   }, [startRecognition]);
 
   const onChangeUsername = (e: any) => {
--- src/App/usecase.ts
+++ src/App/usecase.ts
@@ -1,3 +1,8 @@
+import axios from "axios";
+import SockJS from "sockjs-client";
+import { ControlMessage } from "./model";
+import { useHistory } from "react-router-dom";
+
 declare const webkitSpeechRecognition: typeof SpeechRecognition;
 
 export const speechRecognition = (() => {
@@ -28,4 +33,94 @@ export const speechSynthesis = (text: string) => {
     }
     resolve(false);
   });
+};
+
+export const authCheck = ({
+  loading,
+  history,
+  setUsername,
+  setLoading,
+}: {
+  loading: boolean;
+  history: ReturnType<typeof useHistory>;
+  setUsername: (value: string) => void;
+  setLoading: (value: boolean) => void;
+}) => {
+  const auth = async () => {
+    try {
+      const res = await axios.post("/auth");
+      if (res.data.username) {
+        setUsername(res.data.username);
+      } else {
+        history.push("/login");
+      }
+    } catch (err) {
+      history.push("/login");
+    }
+    setLoading(false);
+  };
+  if (loading) auth();
+};
+
+export const processControl = ({
+  setResult,
+  setStartRecognition,
+}: {
+  setResult: (value: string) => void;
+  setStartRecognition: (value: boolean) => void;
+}) => {
+  const sock = new SockJS("/controller");
+  const onmessage = async (e: MessageEvent) => {
+    const data = JSON.parse(e.data) as ControlMessage;
+    switch (data.action) {
+      case "text-to-speech":
+        setResult(data.utterance);
+        await speechSynthesis(data.utterance);
+        await axios.post("/ready");
+        break;
+      case "speech-to-text/start":
+        setStartRecognition(true);
+        setResult("");
+        break;
+      case "speech-to-text/stop":
+        setStartRecognition(false);
+        setResult("");
+        break;
+    }
+  };
+  sock.addEventListener("message", onmessage);
+  return () => {
+    sock.removeEventListener("message", onmessage);
+  };
+};
+
+export const processRecognition = async ({
+  startRecognition,
+  setResult,
+  setStartRecognition,
+}: {
+  startRecognition: boolean;
+  setResult: (value: string) => void;
+  setStartRecognition: (value: boolean) => void;
+}) => {
+  if (speechRecognition !== undefined) {
+    if (startRecognition) {
+      await axios.post("/recognition", { state: "start" });
+      speechRecognition.onresult = async (e) => {
+        setResult(e.results[0][0].transcript);
+        // 認識結果を送信
+        await axios.post("/transcript", {
+          transcript: e.results[0][0].transcript,
+        });
+      };
+      speechRecognition.onend = async () => {
+        setStartRecognition(false);
+        // 音声認識終了通知
+        await axios.post("/recognition", { state: "end" });
+      };
+      speechRecognition.start();
+    } else {
+      speechRecognition.stop();
+    }
+  }
 };
