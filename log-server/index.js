13084306ab3cebbc4a3c0f8adb2c8fa4c84dee76
Yamaguchi Hiromitsu <yamagame@mac.com>
2021-07-21 18:55:30 +0900

    ログ機能対応

--- log-server/index.js
+++ log-server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const dgram = require("dgram");
 const http = require("http");
 const port = process.env.PORT || 4200;
+
+const socket = dgram.createSocket("udp4");
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -12,6 +15,19 @@ app.use(express.text());
 
 app.post("/logger", (req, res) => {
   console.log(JSON.stringify(req.body, null, "  "));
+  try {
+    const data = JSON.stringify(req.body, null, "  ");
+    socket.send(
+      data,
+      0,
+      Buffer.byteLength(data),
+      7010,
+      "192.168.11.153",
+      (err, bytes) => {
+        if (err) throw err;
+      }
+    );
+  } catch {}
   res.sendStatus(200);
 });
 
